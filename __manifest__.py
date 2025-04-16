e054b37e98b69e9736c814639f7a3f9a5b571ade
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-04-16 07:46:27 -0300

    Initialized module

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fix Analytic report",
+
+    'summary': """
+        Fix Analytic report""",
+
+    'description': """
+        Fix Analytic report
+    """,
+
+    'author': "OutsourceArg",
+    'website': "https://www.outsourcearg.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+}
---
+++ models/models.py
@@ -0,0 +1,18 @@
+from odoo import models, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.onchange('account_id')
+    def _inverse_account_id(self):
+        for record in self:
+            if isinstance(record.id, models.NewId):
+                _logger.info("Operando en registro no guardado")
+                return
+            self._inverse_analytic_distribution()
+            self._conditional_add_to_compute('tax_ids', lambda line: (
+                line.account_id.tax_ids
+                and not line.product_id.taxes_
